49912931d048099311d195e8084125cf5a612f9f
ADIB-the-NOOB <mdadib550@gmail.com>
2022-11-18 01:42:30 +0600

    google Auth Fixed

--- user/urls.py
+++ user/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.userRegister, name='register'),
     path('login/', views.userLogin, name='login'),
     path('logout/', views.userLogout, name='logout'),
     path('register/', views.userRegister, name='register'),
