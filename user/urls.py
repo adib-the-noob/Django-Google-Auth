67f9da3c591bb8514e873bd587721053a36bca74
ADIB-the-NOOB <mdadib550@gmail.com>
2022-11-17 06:20:14 -0800

    User Form Added

--- user/urls.py
+++ user/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.userLogin, name='login'),
+    path('login/', views.userLogin, name='login'),
+    path('register/', views.userRegister, name='register'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
@@ -6,4 +7,12 @@ def userLogin(request):
     return render(request, 'login.html')
 
 def userRegister(request):
-    return render(request, 'register.html')
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {'form':form}
+    return render(request, 'register.html', context)
