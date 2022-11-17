a09a5c1762a43e166015bc17bb488d748f03077c
ADIB-the-NOOB <mdadib550@gmail.com>
2022-11-17 15:32:36 +0600

    Login Form Added

--- user/urls.py
+++ user/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('login/', views.userLogin, name='login'),
     path('register/', views.userRegister, name='register'),
+    path('home/', views.home, name='home'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render,redirect
 from .forms import CreateUserForm
 from django.contrib import messages
+
+from django.contrib.auth import authenticate, login, logout 
 # Create your views here.
 
+
+def home(request):
+    return render(request, 'home.html')
+
+
 def userLogin(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not  None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username OR password is incorrect!')
     return render(request, 'login.html')
+
 
 def userRegister(request):
     form = CreateUserForm()
